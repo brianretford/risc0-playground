53a32fa8c02a82be504518792f96fdf40b378f7a
Brian Retford <bretford@gmail.com>
2022-11-15 13:09:35 -0800

    initial swag

--- methods/build.rs
+++ methods/build.rs
@@ -1,3 +1,13 @@
+use risc0_build::{GuestOptions};
+use std::{collections::HashMap};
+
 fn main() {
-    risc0_build::embed_methods();
+    let inner_pkg_options = GuestOptions {
+        code_limit: 12,
+        features: vec![],
+    };
+
+    let map = HashMap::from([("methods-guest", inner_pkg_options)]);
+    
+    risc0_build::embed_methods_with_options(map);
 }
--- methods/guest/src/bin/method_name.rs
+++ methods/guest/src/bin/method_name.rs
@@ -1,10 +1,28 @@
 // TODO: Rename this file to change the name of this method from METHOD_NAME
 
 #![no_main]
-#![no_std]
 
+pub extern crate externc_libm as libm;
+
+use risc0_zkvm_guest::env;
+use dssim_core::{RGBAPLU, ToRGBAPLU};
+use dssim_core::Dssim;
+use png_decoder::decode;
+use imgref::{Img, ImgVec};
+use rgb::*;
 risc0_zkvm_guest::entry!(main);
 
+fn load() -> ImgVec<RGBAPLU> {
+    let gray1_png_bytes: &[u8] = include_bytes!("../../../../../dssim/tests/gray1-gray.png");
+    let (hdr, gray1_raw) = decode(gray1_png_bytes).unwrap();
+    Img::new(gray1_raw.as_rgba().to_rgbaplu(), hdr.width as usize, hdr.height as usize)
+}
+
 pub fn main() {
-    // TODO: Implement your guest code here
+    let comparer = Dssim::new();
+    let img1 = load();
+    let gray1 = comparer.create_image(&img1).unwrap();
+    let gray2 = comparer.create_image(&img1).unwrap();
+    let score : f64 = f64::from(comparer.compare(&gray1, gray2).0);
+    env::commit(&score);
 }
